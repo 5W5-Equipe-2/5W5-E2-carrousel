05a28c410e07b369db58c1a984e8ab81ff7a22fd
Vincent Hum <111765064+Vythals@users.noreply.github.com>
2023-10-26 18:22:08 -0400

    debut du carrousel

--- carrousel.php
+++ carrousel.php
@@ -1,49 +1,48 @@
 <?php
 /*
-  * Plugin name: Carrousel 5W5
-  * Description: Cette extension carrousel permet d'afficher dans une boîte modale animée les images d'une galerie
-  * Version: 1.0
-  * Author: Noémie da Silva, Victor Desjardins, Vincent Gélinas, Vincent Hum, Dac Anne Nguyen
-  * Author URI: https://github.com/5W5-Equipe-2
-  */
+Plugin Name: Mon Carrousel
+Description: Un carrousel interactif pour afficher des articles de différentes catégories.
+Version: 1.0
+*/
 
-
-function mon_enqueue_css_js()
+function mon_carrousel_shortcode()
 {
-  $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
-  $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+    ob_start();
+?>
+    <div class="mon-carrousel">
+        <div class="carousel-content">
+            <?php
+            // Récupérez les articles de différentes catégories
+            $args = array(
+                'posts_per_page' => 5, // Nombre d'articles à afficher
+                'category_name' => '1j1', // Remplacez par la catégorie souhaitée
+            );
+            $query = new WP_Query($args);
 
-  wp_enqueue_style(
-    '5W5_plugin_carrousel_css',
-    plugin_dir_url(__FILE__) . "style.css",
-    array(),
-    $version_css
-  );
+            while ($query->have_posts()) : $query->the_post();
+            ?>
+                <div class="carousel-item">
+                    <h2><?php the_title(); ?></h2>
+                    <div class="content"><?php the_content(); ?></div>
+                </div>
+            <?php
+            endwhile;
+            wp_reset_postdata();
+            ?>
+        </div>
+        <div class="carousel-navigation">
+            <button class="prev-button">Précédent</button>
+            <span class="article-counter">1/10</span>
+            <button class="next-button">Suivant</button>
+        </div>
 
-  wp_enqueue_script(
-    '5W5_plugin_carrousel_js',
-    plugin_dir_url(__FILE__) . "js/carrousel.js",
-    array(),
-    $version_js,
-    true
-  ); //permet d'ajouter le JS à la fin de la page
+    </div>
 
+
+
+<?php
+    wp_enqueue_script('carrousel-js', plugins_url('js/carrousel.js', __FILE__), array('jquery'), '1.0', true);
+    wp_enqueue_style('carrousel-css', plugins_url('sass/style.scss', __FILE__));
+    return ob_get_clean();
 }
-
-add_action('wp_enqueue_scripts', 'mon_enqueue_css_js');
-
-function creation_carrousel()
-{
-
-  return "<div class='carrousel'>
-            <button class='carrousel__x'>X</button>
-            <div class='fleche carrousel__fleche_gauche'><</div>
-            <div class='fleche carrousel__fleche_droite'>></div>
-            <figure class='carrousel__figure'>
-            
-            </figure>
-            <form class='carrousel__form'></form>
-            </div> <!-- fin du carrousel -->
-    ";
-}
-add_shortcode('5W5_carrousel', 'creation_carrousel');
+add_shortcode('5w5e2carrousel', 'mon_carrousel_shortcode');
