44f6e68a48b7efce4af701a21407e3f39d7e3480
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2023-10-30 12:42:53 -0400

    Carrousel de la branche vince

--- carrousel.php
+++ carrousel.php
@@ -1,5 +1,6 @@
 <?php
 /*
+/*
   * Plugin name: Carrousel 5W5
   * Description: Cette extension carrousel permet d'afficher dans une boîte modale animée les images d'une galerie
   * Version: 1.0
@@ -7,43 +8,63 @@
   * Author URI: https://github.com/5W5-Equipe-2
   */
 
-
-function mon_enqueue_css_js()
-{
-  $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
-  $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
-
-  wp_enqueue_style(
-    '5W5_plugin_carrousel_css',
-    plugin_dir_url(__FILE__) . "style.css",
-    array(),
-    $version_css
-  );
-
-  wp_enqueue_script(
-    '5W5_plugin_carrousel_js',
-    plugin_dir_url(__FILE__) . "js/carrousel.js",
-    array(),
-    $version_js,
-    true
-  ); //permet d'ajouter le JS à la fin de la page
-
-}
-
-add_action('wp_enqueue_scripts', 'mon_enqueue_css_js');
-
-function creation_carrousel()
-{
-
-  return "<div class='carrousel'>
-            <button class='carrousel__x'>X</button>
-            <div class='fleche carrousel__fleche_gauche'><</div>
-            <div class='fleche carrousel__fleche_droite'>></div>
-            <figure class='carrousel__figure'>
-            
-            </figure>
-            <form class='carrousel__form'></form>
-            </div> <!-- fin du carrousel -->
-    ";
-}
-add_shortcode('5W5_carrousel', 'creation_carrousel');
+  function mon_carrousel_shortcode($atts)
+  {
+      // Récupérez les attributs du shortcode
+      $atts = shortcode_atts(array(
+          'categories' => '', // Attribut pour les catégories, vide par défaut
+      ), $atts);
+  
+      // Récupérez les catégories du shortcode
+      $categories = $atts['categories'];
+  
+      // Excluez les catégories si la notation est utilisée
+      $exclude_categories = array();
+      if (strpos($categories, '-') !== false) {
+          $categories_array = explode(',', $categories);
+          foreach ($categories_array as $cat) {
+              if (substr($cat, 0, 1) === '-') {
+                  $exclude_categories[] = ltrim($cat, '-');
+              }
+          }
+          $categories = implode(',', array_diff($categories_array, $exclude_categories));
+      }
+  
+      ob_start();
+  ?>
+      <div class="mon-carrousel">
+          <div class="carousel-content">
+              <?php
+              // Utilisez les catégories dans la requête WP_Query
+              $args = array(
+                  'posts_per_page' => 5, // Nombre d'articles à afficher
+                  'category_name' => $categories,
+                  'category__not_in' => array_map('get_cat_ID', $exclude_categories),
+              );
+              $query = new WP_Query($args);
+  
+              while ($query->have_posts()) : $query->the_post();
+              ?>
+                  <div class="carousel-item">
+                      <h2><?php the_title(); ?></h2>
+                      <div class="content"><?php the_content(); ?></div>
+                  </div>
+              <?php
+              endwhile;
+              wp_reset_postdata();
+              ?>
+          </div>
+          <div class="carousel-navigation">
+              <button class="prev-button">Précédent</button>
+              <span class="article-counter">1/10</span>
+              <button class="next-button">Suivant</button>
+          </div>
+      </div>
+  
+  <?php
+      // Enqueue JavaScript et CSS
+      wp_enqueue_script('carrousel-js', plugins_url('js/carrousel.js', __FILE__), array('jquery'), '1.0', true);
+      wp_enqueue_style('carrousel-css', plugins_url('sass/style.scss', __FILE__));
+      return ob_get_clean();
+  }
+  add_shortcode('5w5e2carrousel', 'mon_carrousel_shortcode');
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,146 +1,47 @@
-(function () {
-  console.log("la galerie");
+document.addEventListener('DOMContentLoaded', function () {
+  const carouselContent = document.querySelector('.carousel-content');
+  const carouselItems = document.querySelectorAll('.carousel-item');
+  let currentItem = 0;
 
-  //Variables pour les classes css du carrousel et la balise html img
-  let carrousel = document.querySelector(".carrousel"),
-    carrouselX = document.querySelector(".carrousel__x"),
-    carrouselFigure = document.querySelector(".carrousel__figure"),
-    carrouselForm = document.querySelector(".carrousel__form"),
-    visuelFlecheGauche = document.querySelector(".carrousel__fleche_gauche"),
-    visuelFlecheDroite = document.querySelector(".carrousel__fleche_droite"),
-    galerie = document.querySelector(".galerie"),
-    galerieImg = galerie.querySelectorAll("img");
+  function showItem(index) {
+      // Masquez l'élément actuel avec un effet de fondu
+      carouselItems[currentItem].style.opacity = 0;
 
-  // Variables pour la position des boutons radios et pour faire avancer les images du carrousel
-  let position = 0;
-  let index = 0;
-  let ancienIndex = null;
+      // Affichez l'élément cible avec un effet de fondu
+      carouselItems[index].style.opacity = 1;
 
-  /******************** Pour fermer le carrousel ********************************/
-  carrouselX.addEventListener("mousedown", function () {
-    carrousel.classList.remove("carrousel-activer");
-    document.removeEventListener("keydown", surveillerTouche);
-  });
-
-  /***************  Ajouter chaque image d'une galerie au carrousel *************/
-  /* -- boucle qui permet construire le carrousel */
-  for (const elt of galerieImg) {
-    elt.dataset.index = position;
-    /* en cliquant sur une image de la galerie */
-    elt.addEventListener("mousedown", function (e) {
-      /*
-      si le carrousel n'est pas déjà ouvert, il faut lui ajouter
-      la classe "carrousel-activer"
-      */
-      if (!elt.classList.contains("carrousel-activer")) {
-        carrousel.classList.add("carrousel-activer");
-      }
-
-      /* Écouteur sur les touches du clavier */
-      document.addEventListener("keydown", surveillerTouche);
-
-      /* Initialiser l'index des images */
-      index = e.target.dataset.index;
-      carrouselForm.children[index].checked = true;
-
-      /* Appel de la fonction pour afficher une image */
-      afficherImageCarrousel();
-
-      /* Écouteur sur les flèches de naviagtion */
-      visuelFlecheGauche.addEventListener("mousedown", reculerImage);
-      visuelFlecheDroite.addEventListener("mousedown", avancerImage);
-    });
-    /* Appel des fonctions pour ajouter des images et des boutons radio */
-    ajouterUneImageDansCaroussel(elt);
-    ajouterUnBoutonRadio();
+      currentItem = index;
+      updateCounter(); // Mettez à jour le compteur d'articles
   }
 
-  /**
-   * Création dynamique d'une image pour le carousel
-   * @param {*} elt une image de la galerie
-   */
-
-  function ajouterUneImageDansCaroussel(elt) {
-    let elImg = `<img src=${elt.src} class="carrousel__img" alt="">`;
-    carrouselFigure.insertAdjacentHTML("beforeend", elImg);
+  function updateCounter() {
+      const counter = document.querySelector('.article-counter');
+      const totalItems = carouselItems.length;
+      const currentNumber = currentItem + 1;
+      counter.textContent = `${currentNumber}/${totalItems}`;
   }
 
-  /**
-   * Fonction pour afficher la nouvelle image du carrousel
-   */
-  function afficherImageCarrousel() {
-    if (ancienIndex != null) {
-      carrouselFigure.children[ancienIndex].style.opacity = "0";
-      carrouselFigure.children[ancienIndex].classList.remove(
-        "carrousel__img-activer"
-      );
-
-      carrouselForm.children[ancienIndex].checked = false;
-    }
-
-    carrouselFigure.children[index].style.opacity = "1";
-    carrouselFigure.children[index].classList.add("carrousel__img-activer");
-
-    carrouselForm.children[index].checked = true;
-    ancienIndex = index;
-
-    /** Afficher les images sans les distortionner */
-    let imgLargeur = carrouselFigure.children[index].naturalWidth,
-      imgHauteur = carrouselFigure.children[index].naturalHeight,
-      ratio = `${(imgHauteur / imgLargeur) * 100}%`;
-
-    document.documentElement.style.setProperty("--ratio", ratio);
+  // Fonction pour afficher l'élément précédent
+  function showPreviousItem() {
+      let index = (currentItem - 1 + carouselItems.length) % carouselItems.length;
+      showItem(index);
   }
 
-  /**
-   * Fonction pour ajouter des boutons radio
-   * et avancer les images
-   */
-
-  function ajouterUnBoutonRadio() {
-    let rad = document.createElement("input");
-    rad.setAttribute("type", "radio");
-    rad.setAttribute("name", "carrousel__rad");
-    rad.classList.add("carrousel__rad");
-    rad.dataset.index = position;
-    rad.addEventListener("mousedown", function () {
-      index = this.dataset.index;
-      afficherImageCarrousel();
-    });
-
-    position = position + 1;
-    carrouselForm.append(rad);
+  // Fonction pour afficher l'élément suivant
+  function showNextItem() {
+      let index = (currentItem + 1) % carouselItems.length;
+      showItem(index);
   }
 
-  /**
-   * Fonctions pour faire avancer ou reculer les images avec les touches du clavier
-   */
+  // Ajoutez un gestionnaire de clic pour le bouton "Précédent"
+  const prevButton = document.querySelector('.prev-button');
+  prevButton.addEventListener('click', showPreviousItem);
 
-  function surveillerTouche(event) {
-    //flèche gauche ou A
-    if (event.keyCode == 37 || event.keyCode == 65) {
-      reculerImage();
-      //flèche droite ou D
-    } else if (event.keyCode == 39 || event.keyCode == 68) {
-      avancerImage();
-    }
-  }
+  // Ajoutez un gestionnaire de clic pour le bouton "Suivant"
+  const nextButton = document.querySelector('.next-button');
+  nextButton.addEventListener('click', showNextItem);
 
-  function reculerImage() {
-    index--;
-    if (index < 0) {
-      index = position - 1;
-    }
-    afficherImageCarrousel();
-    carrouselForm.children[index].checked = true;
-  }
-
-  function avancerImage() {
-    index++;
-    if (index > position - 1) {
-      index = 0;
-    }
-    afficherImageCarrousel();
-    carrouselForm.children[index].checked = true;
-  }
-})();
+  // Démarrez le carrousel en ajoutant la classe 'active' au premier élément
+  carouselItems[currentItem].style.opacity = 1;
+  updateCounter(); // Mettez à jour le compteur d'articles initial
+});
