08036fe601e40195bb20e88df126b7df2727f3af
Vincent Hum <111765064+Vythals@users.noreply.github.com>
2023-12-05 08:50:32 -0500

    debut cleanup code

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,50 +1,50 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const carouselContent = document.querySelector('.carousel-content');
-    const carouselItems = document.querySelectorAll('.carousel-item');
-    let currentItem = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  const carrouselItems = document.querySelectorAll(".carrousel-item");
+  let currentItem = 0;
 
-    function showItem(index) {
-        // Masquez l'élément actuel avec un effet de fondu
-        carouselItems[currentItem].style.opacity = 0;
-        carouselItems[currentItem].style.zIndex = 1;
+  function showItem(index) {
+    // Masquez l'élément actuel avec un effet de fondu
+    carrouselItems[currentItem].style.opacity = 0;
+    carrouselItems[currentItem].style.zIndex = 1;
 
-        // Affichez l'élément cible avec un effet de fondu
-        carouselItems[index].style.opacity = 1;
-        carouselItems[index].style.zIndex = 10;
+    // Affichez l'élément cible avec un effet de fondu
+    carrouselItems[index].style.opacity = 1;
+    carrouselItems[index].style.zIndex = 10;
 
-        currentItem = index;
-        updateCounter(); // Mettez à jour le compteur d'articles
-    }
+    currentItem = index;
+    updateCounter(); // Mettez à jour le compteur d'articles
+  }
 
-    function updateCounter() {
-        const counter = document.querySelector('.article-counter');
-        const totalItems = carouselItems.length;
-        const currentNumber = currentItem + 1;
-        counter.textContent = `${currentNumber}/${totalItems}`;
-    }
+  function updateCounter() {
+    const counter = document.querySelector(".article-counter");
+    const totalItems = carrouselItems.length;
+    const currentNumber = currentItem + 1;
+    counter.textContent = `${currentNumber}/${totalItems}`;
+  }
 
-    // Fonction pour afficher l'élément précédent
-    function showPreviousItem() {
-        let index = (currentItem - 1 + carouselItems.length) % carouselItems.length;
-        showItem(index);
-    }
+  // Fonction pour afficher l'élément précédent
+  function showPreviousItem() {
+    let index =
+      (currentItem - 1 + carrouselItems.length) % carrouselItems.length;
+    showItem(index);
+  }
 
-    // Fonction pour afficher l'élément suivant
-    function showNextItem() {
-        let index = (currentItem + 1) % carouselItems.length;
-        showItem(index);
-    }
+  // Fonction pour afficher l'élément suivant
+  function showNextItem() {
+    let index = (currentItem + 1) % carrouselItems.length;
+    showItem(index);
+  }
 
-    // Ajoutez un gestionnaire de clic pour le bouton "Précédent"
-    const prevButton = document.querySelector('.prev-button');
-    prevButton.addEventListener('click', showPreviousItem);
+  // Ajoutez un gestionnaire de clic pour le bouton "Précédent"
+  const prevButton = document.querySelector(".prev-button");
+  prevButton.addEventListener("click", showPreviousItem);
 
-    // Ajoutez un gestionnaire de clic pour le bouton "Suivant"
-    const nextButton = document.querySelector('.next-button');
-    nextButton.addEventListener('click', showNextItem);
+  // Ajoutez un gestionnaire de clic pour le bouton "Suivant"
+  const nextButton = document.querySelector(".next-button");
+  nextButton.addEventListener("click", showNextItem);
 
-    // Démarrez le carrousel en ajoutant la classe 'active' au premier élément
-    carouselItems[currentItem].style.opacity = 1;
-    carouselItems[currentItem].style.zIndex = 10;
-    updateCounter(); // Mettez à jour le compteur d'articles initial
+  // Démarrez le carrousel en ajoutant la classe 'active' au premier élément
+  carrouselItems[currentItem].style.opacity = 1;
+  carrouselItems[currentItem].style.zIndex = 10;
+  updateCounter(); // Mettez à jour le compteur d'articles initial
 });
